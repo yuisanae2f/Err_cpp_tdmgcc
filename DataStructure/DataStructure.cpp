2321b299b7d6925587f44e440452d6025f4cc0cc
Yui San ae2f <84163330+yuisanae2f@users.noreply.github.com>
2024-07-13 05:07:00 +0900

    초기 커밋.

---
+++ DataStructure/DataStructure.cpp
@@ -0,0 +1,39 @@
+﻿// DataStructure.cpp : 애플리케이션의 진입점을 정의합니다.
+//
+
+#include <iostream>
+#include <vector>
+
+namespace ae2f {
+	struct iAllocator {
+		virtual void* ptr() = 0;
+	};
+
+	template<typename t>
+	struct cAllocator : public iAllocator {
+		t&& raw;
+		virtual void* ptr() override { return ((void*)&raw); }
+		cAllocator(t& val) : raw((t&&)val) {
+			const size_t _amp = sizeof(val);
+			const size_t _div = sizeof(val[0]);
+			for (size_t i = 0; i < (_amp / _div); i++) {
+				raw[i] = val[i];
+			} 
+		}
+		cAllocator(t&& val) : raw((t&&)val) {
+			const size_t _amp = sizeof(val);
+			const size_t _div = sizeof(val[0]);
+
+			for (size_t i = 0; i < (_amp / _div); i++) {
+				raw[i] = val[i];
+			}
+		}
+	};
+}
+
+int main()
+{
+	ae2f::cAllocator<int[3]> a({ 3, 65, 5 });
+	std::cout << a.raw[1];
+	return 0;
+}
